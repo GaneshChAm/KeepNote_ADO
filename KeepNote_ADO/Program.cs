20d03934cdafa620e0282dfcb06519d192e6962d
Gopu <ganeshsivasai.gopu@dxc.com>
2023-05-11 13:57:48 +0530

    initial commit

---
+++ KeepNote_ADO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KeepNote_ADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
